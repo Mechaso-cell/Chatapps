502a5644aa916b1d0a6ef763621140cd3cd4112f
Christian Joseph <amores.cj.bsmath@gmail.com>
2026-01-29 16:53:43 +0800

    Fix lib/utls

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,10 +6,10 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: falseprocess.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+    httpOnly: true, // prevent XSS attacks
+    sameSite: "strict", // prevent CSRF attacks
+    secure: process.env.NODE_ENV !== "development", // true in production
   });
 
   return token;
